e3fd731ae179d9f61f46ceee14f134a2d8e0cb34
pscn <pschnebel@gmail.com>
2019-05-10 23:34:42 +0200

    Add cli options, middleware and move route setup

--- main.go
+++ main.go
@@ -8,28 +8,36 @@ import (
 	"github.com/gorilla/mux"
 
 	"github.com/pscn/flavor2go/api"
+	"github.com/pscn/flavor2go/middleware"
 	"github.com/pscn/flavor2go/model"
 
 	"github.com/karrick/golf"
 )
 
-var port = golf.IntP('p', "port", 3000, "port to listen on")
+var port = golf.IntP('l', "listen", 3000, "port to listen on")
+
+// FIXME: maybe just use a connect string
+// FIXME:² read from config
+var pgHost = golf.StringP('h', "host", "localhost", "postgres host")
+var pgPort = golf.IntP('p', "port", 5432, "postgres port")
+var pgDatabase = golf.StringP('d', "database", "gusta", "postgres database")
+var pgUser = golf.StringP('u', "user", "gusta", "postgres user")
+var pgPass = golf.String("pass", "changeme", "postgres password")
+var pgUseSSL = golf.BoolP('s', "ssl", false, "postgres use SSL")
 
 func main() {
 	golf.Parse()
 	log.SetFlags(0)
 
-	m := model.NewPostgres("localhost", 5432, "gusta", "gusta", "changeme", false)
+	m := model.NewPostgres(*pgHost, *pgPort, *pgUser, *pgDatabase, *pgPass, *pgUseSSL)
 	a := api.New(m)
 	defer m.Close()
+
 	r := mux.NewRouter()
-	r.HandleFunc("/vendor/", a.CreateVendor).Methods("POST")
-	r.HandleFunc("/vendor/", a.UpdateVendor).Methods("PUT")
-	r.HandleFunc("/vendor/{uuid}", a.UpdateVendor).Methods("PUT")
-	r.HandleFunc("/vendor/{uuid}", a.GetVendorByUUID).Methods("POST")
-	r.HandleFunc("/flavor/{uuid}", a.GetFlavorByUUID).Methods("GET")
+	a.Register(r)
 
+	r.Use(middleware.Log)
 	addr := fmt.Sprintf(":%d", *port)
+	log.Printf("listen on: %d", *port)
 	log.Fatal(http.ListenAndServe(addr, r))
-
 }
