943fd7da2f9b9cdd52d965931ff0ec1056111fd0
pscn <pschnebel@gmail.com>
2019-05-10 23:35:11 +0200

    Add CRUD

--- api/flavor.go
+++ api/flavor.go
@@ -8,13 +8,63 @@ import (
 	"github.com/pscn/flavor2go/model"
 )
 
+// CreateFlavor -
+func (a *API) CreateFlavor(w http.ResponseWriter, r *http.Request) {
+	flavor := &model.Flavor{}
+	err := json.NewDecoder(r.Body).Decode(flavor)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if err = a.m.AddFlavor(flavor); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(Error(err))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(flavor)
+}
+
+// UpdateFlavor -
+func (a *API) UpdateFlavor(w http.ResponseWriter, r *http.Request) {
+	flavor := &model.Flavor{}
+	err := json.NewDecoder(r.Body).Decode(flavor)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	vars := mux.Vars(r)
+	if vars["uuid"] != "" {
+		flavor.UUID = vars["uuid"]
+	}
+
+	if err = a.m.UpdateFlavor(flavor); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(Error(err))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(flavor)
+}
+
+// GetFlavorByUUID -
 func (a *API) GetFlavorByUUID(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	flavor := a.m.GetFlavor(&model.Flavor{UUID: vars["uuid"]})
+	flavor := a.m.GetFlavor(vars["uuid"])
 	if flavor.UUID == "" {
 		w.WriteHeader(http.StatusNotFound)
 	} else {
 		w.WriteHeader(http.StatusOK)
 	}
 	json.NewEncoder(w).Encode(flavor)
+}
+
+// GetFlavors -
+func (a *API) GetFlavors(w http.ResponseWriter, r *http.Request) {
+	flavors := a.m.GetFlavors()
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(flavors)
 }
