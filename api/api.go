d6186e7195b42e4a3ea1a83cfcfc8729ef4f50e5
pscn <pschnebel@gmail.com>
2019-05-10 23:34:52 +0200

    Add route setup

--- api/api.go
+++ api/api.go
@@ -1,6 +1,9 @@
 package api
 
-import "github.com/pscn/flavor2go/model"
+import (
+	"github.com/gorilla/mux"
+	"github.com/pscn/flavor2go/model"
+)
 
 type API struct {
 	m *model.Manager
@@ -8,4 +11,20 @@ type API struct {
 
 func New(m *model.Manager) *API {
 	return &API{m: m}
+}
+
+// Register routes
+func (a *API) Register(r *mux.Router) {
+	r.HandleFunc("/vendor/", a.CreateVendor).Methods("POST")
+	r.HandleFunc("/vendor/", a.UpdateVendor).Methods("PUT")
+	r.HandleFunc("/vendor/{uuid}", a.UpdateVendor).Methods("PUT")
+	r.HandleFunc("/vendor/{uuid}", a.GetVendorByUUID).Methods("GET")
+	r.HandleFunc("/vendors/", a.GetVendors).Methods("GET")
+
+	r.HandleFunc("/flavor/", a.CreateFlavor).Methods("POST")
+	r.HandleFunc("/flavor/", a.UpdateFlavor).Methods("PUT")
+	r.HandleFunc("/flavor/{uuid}", a.UpdateFlavor).Methods("PUT")
+	r.HandleFunc("/flavor/{uuid}", a.GetFlavorByUUID).Methods("GET")
+	r.HandleFunc("/flavors/", a.GetFlavors).Methods("GET")
+
 }
