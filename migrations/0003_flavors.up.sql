2d697b62738f1e62d53e7dce67f12e18b4111c78
pscn <pschnebel@gmail.com>
2019-05-11 00:27:10 +0200

    Fix typo

--- migrations/0003_flavors.up.sql
+++ migrations/0003_flavors.up.sql
@@ -1,8 +1,8 @@
-CREATE TABLE flavor (
+CREATE TABLE flavors (
   uuid UUID NOT NULL,
   vendor_uuid UUID NOT NULL,
   name VARCHAR(255) NOT NULL,
   CONSTRAINT flavor_pk PRIMARY KEY (uuid),
   CONSTRAINT flavor_name_vendor_uq UNIQUE (vendor_uuid, name),
-  CONSTRAINT favlor_vendor_fk FOREIGN KEY (vendor_uuid) REFERENCES vendors (uuid)
+  CONSTRAINT flavor_vendor_fk FOREIGN KEY (vendor_uuid) REFERENCES vendors (uuid)
 );
