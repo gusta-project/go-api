8c2f55d0a775f3cddd52a2c0b6bdf9da51fdecb0
pscn <pschnebel@gmail.com>
2019-05-11 10:50:25 +0200

    Add Method to log

--- middleware/log.go
+++ middleware/log.go
@@ -9,7 +9,7 @@ import (
 func Log(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// FIXME: prettier & more informative
-		log.Printf("%s %s", r.RemoteAddr, r.RequestURI)
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.RequestURI)
 		next.ServeHTTP(w, r)
 	})
 }
